df9421ea388cf6e03bcda30c637c31f8b4d8293e
Enric Vergara <donkikochan@gmail.com>
2024-10-16 08:33:08 +0200

    refactoring code for testing

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -7,28 +7,7 @@ export default function HomeScreen({ navigation }) {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <Text>Home Screen</Text>
-      <Text>User: </Text>
-      <TextInput
-        style={{
-          height: 40,
-          borderColor: 'gray',
-          borderWidth: 1,
-          width: '80%',
-          marginBottom: 20,
-          paddingHorizontal: 10
-        }}
-        placeholder="Enter your text"
-        value={userText}
-        onChangeText={text => setUserText(text)} // Actualitza el text quan l'usuari escriu
-      />
-      <Button
-        title="Go to Page1"
-        onPress={() => navigation.navigate('Page1', { userText })} // Passa el text com a paràmetre
-      />
-      <Button
-        title="Go to Page 2"
-        onPress={() => navigation.navigate('Page2')}
-      />
+      
     </View>
   );
 }
--- screens/Page1.js
+++ screens/Page1.js
@@ -7,15 +7,7 @@ export default function Page1({ route, navigation }) {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <Text>Page1 Screen</Text>
-      <Text>User input: {userText}</Text>
-      <Button
-        title="Go to Details"
-        onPress={() => navigation.navigate('Details')}
-      />
-      <Button
-        title="Go back"
-        onPress={()=> navigation.goBack()}
-      />
+      
     </View>
   );
 }
